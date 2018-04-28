16b4c002996268b93c3157a1ea0228039fe4a15a
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-04-28 10:05:55 -0700

    ResearchPal support

---
+++ Source/ResearchPal/HideCurrent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using Harmony;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using ResearchPal;
+
+namespace Random_Research.ResearchPal
+{
+	[HarmonyPatch(typeof(Node), "Draw")]
+	public class HideCurrent
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo ProgressPercentInfo = AccessTools.Property(typeof(ResearchProjectDef), "ProgressPercent").GetGetMethod();
+
+			MethodInfo HideProgressPercentInfo = AccessTools.Method(typeof(HideCurrent), "HideProgressPercent");
+
+			foreach (CodeInstruction i in instructions)
+			{
+				yield return i;
+
+				if (i.opcode == OpCodes.Callvirt && i.operand == ProgressPercentInfo)
+					yield return new CodeInstruction(OpCodes.Call, HideProgressPercentInfo);
+			}
+		}
+		
+		public static float HideProgressPercent(float progress)
+		{
+			return BlindResearch.CanSeeCurrent() ? progress : 0;
+		}
+	}
+}
---
+++ Source/ResearchPal/PreventChoice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using Harmony;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using ResearchPal;
+
+namespace Random_Research.ResearchPal
+{
+	[HarmonyPatch(typeof(Node), "Draw")]
+	public class PreventChoice
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo ButtonInvisibleInfo = AccessTools.Method(typeof(Widgets), "ButtonInvisible");
+
+			MethodInfo HideButtonInvisibleInfo = AccessTools.Method(typeof(PreventChoice), "HideButtonInvisible");
+
+			foreach (CodeInstruction i in instructions)
+			{
+				if (i.opcode == OpCodes.Call && i.operand == ButtonInvisibleInfo)
+					i.operand = HideButtonInvisibleInfo;
+				
+				yield return i;
+			}
+		}
+
+		public static bool HideButtonInvisible(Rect butRect, bool doMouseoverSound)
+		{
+			return BlindResearch.CanChangeCurrent() ? Widgets.ButtonInvisible(butRect, doMouseoverSound) : false;
+		}
+	}
+}
