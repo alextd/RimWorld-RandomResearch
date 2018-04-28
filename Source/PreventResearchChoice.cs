781c4155c3b893c3250c42bba5b8936583e8e5fe
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-04-28 07:06:01 -0700

    Remove button to choose research

---
+++ Source/PreventResearchChoice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using Harmony;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace Random_Research
+{
+	[HarmonyPatch(typeof(MainTabWindow_Research), "DrawLeftRect")]
+	class PreventResearchChoice
+	{
+		//private void DrawLeftRect(Rect leftOutRect)
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo ButtonTextInfo = AccessTools.Method(typeof(Widgets), "ButtonText", new Type[]
+				{typeof(Rect), typeof(string), typeof(bool), typeof(bool), typeof(bool)});
+
+			MethodInfo HideButtonTextInfo = AccessTools.Method(typeof(PreventResearchChoice), "HideButtonText");
+
+			foreach (CodeInstruction i in instructions)
+			{
+				if (i.opcode == OpCodes.Call && i.operand == ButtonTextInfo)
+					i.operand = HideButtonTextInfo;
+				yield return i;
+			}
+		}
+
+		public static bool HideButtonText(Rect rect, string label, bool drawBackground, bool doMouseoverSound, bool active)
+		{
+			return false;
+			//return Widgets.ButtonText(rect, label, drawBackground, doMouseoverSound, active);//
+		}
+	}
+}
