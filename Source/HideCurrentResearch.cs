0c8caf31ca4f2afaa5c5a19072f8f9e3b079a703
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-04-28 07:49:48 -0700

    Hide current research when page is opened

---
+++ Source/HideCurrentResearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using Verse;
+using RimWorld;
+
+namespace Random_Research
+{
+	[HarmonyPatch(typeof(MainTabWindow_Research), "PreOpen")]
+	class HideCurrentResearch
+	{
+		//public override void PreOpen()
+		public static void Postfix(MainTabWindow_Research __instance)
+		{
+			AccessTools.Field(typeof(MainTabWindow_Research), "selectedProject").SetValue(__instance, null);
+			//__instance.selectedProject = null;
+			AccessTools.Property(typeof(MainTabWindow_Research), "CurTab").GetSetMethod(true).Invoke(__instance, new object[] { ResearchTabDefOf.Main });
+			//__instance.CurTab = ResearchTabDefOf.Main;
+		}
+	}
+}
