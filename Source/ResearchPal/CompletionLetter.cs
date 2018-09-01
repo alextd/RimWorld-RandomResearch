1eb6a0d0ca7106b21ac43c4ec4f7f092ea8affc9
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-09-01 07:02:46 -0700

    Make sure ResearchPal shows the letter when it's technically not in the queue.

---
+++ Source/ResearchPal/CompletionLetter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace Random_Research.ResearchPal
+{
+	class CompletionLetter
+	{
+		[HarmonyPatch(typeof(ResearchManager), "FinishProject")]
+		[HarmonyPriority(Priority.First)]
+		public class DoCompletionDialog
+		{
+			//Restore completion letter when there's no queue
+			//DoCompletionLetter should probably go in ResearchPal inside TryStartNext but who wants to transpile that or tell them to fix it
+			//public void FinishProject(ResearchProjectDef proj, bool doCompletionDialog = false, Pawn researcher = null)
+			static void Prefix(ResearchProjectDef proj, bool doCompletionDialog)
+			{
+				if (!doCompletionDialog) return;
+
+				MethodInfo CompletionLetterInfo = AccessTools.Method(typeof(global::ResearchPal.Queue), "DoCompletionLetter");
+				if (CompletionLetterInfo == null) return;
+
+				CompletionLetterInfo.Invoke(null, new object[] { proj, null});
+			}
+		}
+	}
+}
